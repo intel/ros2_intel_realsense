cc9df39c3f0a4f173584b0320e5965846d1c262b
Andreas Ziegler <a.ziegler@mt-robot.com>
2019-07-09 16:00:18 +0200

    Rm dead code.

--- realsense_ros2_camera/include/realsense_ros2_camera/realsense_camera_node.hpp
+++ realsense_ros2_camera/include/realsense_ros2_camera/realsense_camera_node.hpp
@@ -1469,15 +1469,3 @@ private:
   rs2::frameset _aligned_frameset;
 };  // end class
 }  // namespace realsense_ros2_camera
-
-/*
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<realsense_ros2_camera::RealSenseCameraNode>();
-  node->onInit();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
-}
-*/
